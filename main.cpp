a37aa5d5ba9938425dc93aa3708fdccbe39f4fa7
RobAndonie <a01411863@tec.mx>
2023-04-13 13:26:26 -0600

    Read line by line

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
-#include <iostream>;
+#include <iostream>
+#include <fstream>
 
+using namespace std;
+
+int readLine(ifstream &file) {
+    string line;
+
+    while (getline(file, line)) {
+        getToken(line);
+    }
+}
+
+int getToken(string line) {
+    
+}
 
 int main() {
+    ifstream file;
+    file.open("test.cpp");
+    readLine(file);
+
 };
