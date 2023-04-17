a323974e25b2dd1e25bcf10e08b9c49c265ceb83
RobAndonie <a01411863@tec.mx>
2023-04-17 11:07:37 -0600

    Updates on main.cpp

--- main.cpp
+++ main.cpp
@@ -10,16 +10,17 @@ vector<regex> dataTypeRegexVec = {
     regex("double"),
     regex("char"),
     regex("float"),
-    regex("bool")};
+    regex("bool"),
+    regex("string")};
 
 vector<regex> keywordsRegexVec = {
     regex("main"),
     regex("return")};
 
-vector<regex> cyclesRegexVec = {
+vector<regex> loopsRegexVec = {
     regex("for"),
     regex("while"),
-    regex("do")};
+    regex("\bdo\b")};
 
 vector<regex> conditionalsRegexVec = {
     regex("if"),
@@ -63,21 +64,170 @@ void sendTokenToHTML(string token)
     outputFile.close();
 }
 
-void getDataType(string line) {
-    for (regex dataTypeRegex : dataTypeRegexVec)
-    {
-        if (regex_search(line, regex("int"))) sendTokenToHTML("int");
-    }
+void getDataTypeToken(string line)
+{
+    if (regex_search(line, regex("int")))
+        sendTokenToHTML("int");
+
+    if (regex_search(line, regex("double")))
+        sendTokenToHTML("double");
+
+    if (regex_search(line, regex("char")))
+        sendTokenToHTML("char");
+
+    if (regex_search(line, regex("float")))
+        sendTokenToHTML("float");
+
+    if (regex_search(line, regex("bool")))
+        sendTokenToHTML("bool");
+
+    if (regex_search(line, regex("string")))
+        sendTokenToHTML("string");
+}
+
+void getKeywordToken(string line)
+{
+    if (regex_search(line, regex("main")))
+        sendTokenToHTML("main");
+
+    if (regex_search(line, regex("return")))
+        sendTokenToHTML("return");
+}
+
+void getLoopToken(string line)
+{
+    if (regex_search(line, regex("for")))
+        sendTokenToHTML("for");
+
+    if (regex_search(line, regex("while")))
+        sendTokenToHTML("while");
+
+    if (regex_search(line, regex("do")))
+        sendTokenToHTML("do");
+}
+
+void getConditionalToken(string line)
+{
+    if (regex_search(line, regex("if")))
+        sendTokenToHTML("if");
+
+    if (regex_search(line, regex("else")))
+        sendTokenToHTML("else");
+
+    if (regex_search(line, regex("switch")))
+        sendTokenToHTML("switch");
+
+    if (regex_search(line, regex("case")))
+        sendTokenToHTML("case");
+
+    if (regex_search(line, regex("default")))
+        sendTokenToHTML("default");
+}
+
+void getOperatorToken(string line)
+{
+    if (regex_search(line, regex("\\+")))
+        sendTokenToHTML("+");
+
+    if (regex_search(line, regex("\\-")))
+        sendTokenToHTML("-");
+
+    if (regex_search(line, regex("\\*")))
+        sendTokenToHTML("*");
+
+    if (regex_search(line, regex("\\/")))
+        sendTokenToHTML("/");
+
+    if (regex_search(line, regex("\\%")))
+        sendTokenToHTML("%");
+
+    if (regex_search(line, regex("\\=")))
+        sendTokenToHTML("=");
+
+    if (regex_search(line, regex("\\+\\+")))
+        sendTokenToHTML("++");
+
+    if (regex_search(line, regex("\\-\\-")))
+        sendTokenToHTML("--");
+
+    if (regex_search(line, regex("\\!")))
+        sendTokenToHTML("!");
+
+    if (regex_search(line, regex("\\&\\&")))
+        sendTokenToHTML("&&");
+
+    if (regex_search(line, regex("\\|\\|")))
+        sendTokenToHTML("||");
+
+    if (regex_search(line, regex("\\<")))
+        sendTokenToHTML("<");
+
+    if (regex_search(line, regex("\\>")))
+        sendTokenToHTML(">");
+
+    if (regex_search(line, regex("\\<\\=")))
+        sendTokenToHTML("<=");
+
+    if (regex_search(line, regex("\\>\\=")))
+        sendTokenToHTML(">=");
+
+    if (regex_search(line, regex("\\!\\=")))
+        sendTokenToHTML("!=");
+
+    if (regex_search(line, regex("\\=\\=")))
+        sendTokenToHTML("==");
+
+    if (regex_search(line, regex("\\+\\=")))
+        sendTokenToHTML("+=");
+
+    if (regex_search(line, regex("\\-\\=")))
+        sendTokenToHTML("-=");
+
+    if (regex_search(line, regex("\\*\\=")))
+        sendTokenToHTML("*=");
+
+    if (regex_search(line, regex("\\/\\=")))
+        sendTokenToHTML("/=");
+
+    if (regex_search(line, regex("\\%\\=")))
+        sendTokenToHTML("%=");
 }
 
 void getToken(string line)
 {
+    if (regex_search(line, regex("int"|"double")))
+        getDataTypeToken(line);
 
-    for (regex dataTypeRegex : dataTypeRegexVec)
+    /**for (regex dataTypeRegex : dataTypeRegexVec)
     {
         if (regex_search(line, dataTypeRegex))
-            getDataType(line);
+            getDataTypeToken(line);
     }
+
+    for (regex keywordRegex : keywordsRegexVec)
+    {
+        if (regex_search(line, keywordRegex))
+            getKeywordToken(line);
+    }
+
+    for (regex cycleRegex : loopsRegexVec)
+    {
+        if (regex_search(line, cycleRegex))
+            getLoopToken(line);
+    }
+
+    for (regex loopsRegex : conditionalsRegexVec)
+    {
+        if (regex_search(line, loopsRegex))
+            getConditionalToken(line);
+    }
+
+    for (regex operatorsRegex : operatorsRegexVec)
+    {
+        if (regex_search(line, operatorsRegex))
+            getOperatorToken(line);
+    }
+    */
 }
 
 void readLine(ifstream &file)
